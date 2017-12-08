df88dce7c39d881b76c0b51c9a93473460a2ae1b
gitKirkChan <kirkchn@gmail.com>
2017-12-07 19:44:50 -0600

    Updated pom; removed bad compiler plugin configs

--- notes-api/src/main/java/com/kchan/project/beyond/notes/domain/Config.java
+++ notes-api/src/main/java/com/kchan/project/beyond/notes/domain/Config.java
@@ -2,5 +2,9 @@ package com.kchan.project.beyond.notes.domain;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/*
+ * Scans for Spring Data repositories within the package of this annotated class
+ * ... Seems to work without though. Why Spring?
+ */
 @EnableJpaRepositories
 public class Config {}
